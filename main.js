26d3eece3a4788e0b82e0c1b902019776719a8a8
maheshmadmax <30284457+maheshmadmax@users.noreply.github.com>
2020-05-07 09:26:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello mahesh\n");
+  response.end("Hello Welcome to Jenkins Lab\n");
 });
 
 // listen on localhost:8000
